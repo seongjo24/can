308d9214a4ab6f63e00b378b9e6ced8c291fd9f0
kangsj24 <kangsj24@naver.com>
2019-11-18 12:19:21 +0900

    first

---
+++ src/main/java/com/can/web/cmm/WebConfig.java
@@ -0,0 +1,25 @@
+package com.can.web.cmm;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] {"/"};
+	}
+
+}
---
+++ src/main/webapp/resources/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var app = app || {};
+app = (()=>{
+        
+        let run=x=>{	
+        	alert('성조')
+      
+        }
+        return {run}
+})();
